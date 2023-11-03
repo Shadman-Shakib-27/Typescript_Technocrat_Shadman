5b76f719e1228a4b2ddfe3fa8545a54fb83bda99
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-11-04 02:08:20 +0600

    2.3--> Intro

---
+++ Module2/2.3.ts
@@ -0,0 +1,14 @@
+{
+   //
+
+   //Generic Type--> Amra Jodi Kono Type Ke Generalize Korte Pari Orthat Dynamic Vabe Jekono Somoye Amader Proyojon Onujayi Type Toiri Korte Pari & Reuse Korte Pari Seitai Generic Type..
+
+
+   
+
+
+
+
+
+   //
+}
