0701f09116556809457e245a75efb2e87f89cf71
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-11-05 16:00:33 +0600

    2.5-->Done

---
+++ Module2/2.4.ts
@@ -0,0 +1,10 @@
+{
+   
+   //Generic with Interface
+
+
+   //Apadoto Bad But Lagle --> 2.4 Video
+
+   //
+
+}
---
+++ Module2/2.5.ts
@@ -0,0 +1,32 @@
+{
+   //  Function With Generics
+   const createArrayWithGenerics=<T>(param:T):T[]=>{
+      return [param]
+   }
+   const resString=createArrayWithGenerics<boolean>(true);
+   //Type Alias
+   type user={
+      name:string,
+      age:number};
+      
+   const resGenenricObj=createArrayWithGenerics<user>({name:"Shadman",age:50})
+
+   // Create Arrray With Tuple
+
+   const createArrayWithGenericTuple=<T,Q>(param1:T,param2:Q):[T,Q]=>{
+      return [param1,param2]
+   }  
+   const resString1=createArrayWithGenericTuple<string,string>("Shadman","Shakib");
+   const res2=createArrayWithGenericTuple<string,{name:string,age:number}>("Shakib",{name:"Shakib",age:50});
+
+   const addCourseToStudent=<T>(student:T)=>{
+      const course= "Next Level Web Dev."
+      return {
+         ...student,course
+      }
+   }
+   const res10=addCourseToStudent({name:"Shadman",age:20,devType:"NLWD"})
+   const res11=addCourseToStudent({name:"Shakib",age:20,devType:"NLWDM"})
+   console.log(res10,res11);
+   //
+}
