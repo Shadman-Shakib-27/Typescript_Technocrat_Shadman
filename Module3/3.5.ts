8a6114b5651994a5756e27e3692182b7ad59751f
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-11-07 17:35:25 +0600

    3.5-->Done

--- Module3/3.5.ts
+++ Module3/3.5.ts
@@ -1,18 +1,41 @@
 {
-   //Accesss Modifier --> Typescript support four types of access modifiers: public, private, protected and read-only. Access modifiers are used to encapsulate class and its member variables.
+  //Accesss Modifier --> Typescript support four types of access modifiers: public, private, protected and read-only. Access modifiers are used to encapsulate class and its member variables.
 
-   
+  class BankAccount {
+    public readonly id: number;
+    public name: string;
+    protected _balance: number;
 
+    constructor(id: number, name: string, _balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = _balance;
+    }
 
+    addMoney(amount: number) {
+      this._balance = this._balance + amount;
+    }
 
+    getBalance() {
+      return this._balance;
+    }
+  }
 
+  class StudentAccount extends BankAccount {
+    test() {
+      this._balance;
+    }
 
+    // constructor(){
 
+    // }
+  }
 
+  const goribManusherAccount = new BankAccount(111, "Mr.Gorib", 250);
+  goribManusherAccount.addMoney(20);
+  goribManusherAccount.getBalance();
 
+  console.log(goribManusherAccount);
 
-
-
-
-   //
+  //
 }
