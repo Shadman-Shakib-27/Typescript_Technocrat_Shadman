8ec622531aab653cb1187384a86e159b260a31f4
Shadman-Shakib-27 <s.s.a.27.8.1999@gmail.com>
2023-11-07 17:01:16 +0600

    3.5-->Start

---
+++ Module3/3.5.ts
@@ -0,0 +1,18 @@
+{
+   //Accesss Modifier --> Typescript support four types of access modifiers: public, private, protected and read-only. Access modifiers are used to encapsulate class and its member variables.
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+   //
+}
